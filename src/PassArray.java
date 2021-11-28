0fd39bb6376a415af0688b13b3ea276d26e66bca
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 16:29:33 -0500

    adjusted spacing

--- src/PassArray.java
+++ src/PassArray.java
@@ -11,14 +11,14 @@ public class PassArray {
 
 		// output original array elements
 		for (int value : array)
-			System.out.printf("   %d", value);
+			System.out.printf("  %d", value);
 
 		modifyArray(array); // pass array reference
 		System.out.printf("%n%nThe values of the modified array are:%n");
 
 		// output modified array elements
 		for (int value : array)
-			System.out.printf("    %d", value);
+			System.out.printf("  %d", value);
 
 		System.out.printf("%n%nEffects of passing array element value: %n" + "array [3] before modifyElement: %d%n",
 				array[3]);
