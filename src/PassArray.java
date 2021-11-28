bdda75beb3d85532a8f5702462396f59e0c6611e
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 16:12:39 -0500

    Java Practice: passing arrays

---
+++ src/PassArray.java
@@ -0,0 +1,11 @@
+//Fig. 7.13: PassArray.java
+//Passing arrays and individual array elements to methods
+
+public class PassArray {
+	// main creates array and calls modifyArray and modifyElement
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
