38603499d00ee08e8494f54a740fb343c7eaf09d
Mike Knoles <MKnoles78@gmail.com>
2021-12-01 23:24:21 -0500

    added arrays with values

--- src/TwoDimensionalArrays.java
+++ src/TwoDimensionalArrays.java
@@ -1,8 +1,9 @@
-
+//Initializing two dimensional-arrays.
 public class TwoDimensionalArrays {
-
+	// create and output two-dimensional arrays
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] array1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+		int[][] array2 = { { 1, 2 }, { 3 }, { 4, 5, 6 } };
 
 	}
 
